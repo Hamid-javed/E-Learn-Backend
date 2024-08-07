201ffdbda985667a44b494adafd860e93a1f5b3e
arbab57 <165644659+arbab57@users.noreply.github.com>
2024-08-07 15:30:27 +0500

    just formatting code

--- routes/useRouter.js
+++ routes/useRouter.js
@@ -1,18 +1,11 @@
-const router = require('express').Router()
-const authControll = require("../controller/authControl")
+const router = require("express").Router();
+const authControll = require("../controller/authControl");
 
+router.get("/", (req, res) => {
+  res.send("hello");
+});
 
+router.post("/register", authControll.register);
+router.post("/login", authControll.login);
 
-router.get("/", (req ,res) => {
-    res.send("hello")
-}
-)
-
-
-router.post("/register", authControll.register)
-
-router.post("/login", authControll.login)
-
-
-
-module.exports = router
+module.exports = router;
