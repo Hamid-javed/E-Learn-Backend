83a2ac8116bc97a0477927b8b5e012559464ef2a
arbab57 <165644659+arbab57@users.noreply.github.com>
2024-08-13 11:00:44 +0500

    added a few apis, for mentor, courses in admin section

--- models/videoSchema.js
+++ models/videoSchema.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
 const videoSchema = new mongoose.Schema({
+title: {type: String, required: true},
 link: {type: String, required: true},
 });
 
--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -1,22 +1,33 @@
 const adminControl = require('../controller/adminControl')
+const mentorControl = require('../controller/mentorControl')
+const courseControl = require("../controller/courseControl")
 const Admin = require("../models/adminSchema")
 const { verifyAdminToken } = require('../middleware/authAdmin')
 const router = require('express').Router()
 
 router.post("/login", adminControl.Login)
+router.get("/mentors/:mentorId/get", verifyAdminToken, mentorControl.getMentor)
+router.get("/mentors/get", verifyAdminToken, adminControl.getMentors)
 router.post("/mentors/add",verifyAdminToken, adminControl.addMentor)
 router.patch("/mentors/:mentorId/update",verifyAdminToken, adminControl.updataMentor)
 router.delete("/mentors/:mentorId/delete",verifyAdminToken, adminControl.deleteMentor)
 router.post("/mentors/:mentorId/add-course/:courseId",verifyAdminToken, adminControl.addMentorCourses)
-router.delete("/mentors/:mentorId/add-course/:courseId",verifyAdminToken, adminControl.deleteMentorCourses)
+router.delete("/mentors/:mentorId/del-course/:courseId",verifyAdminToken, adminControl.deleteMentorCourses)
+router.delete("/mentors/:mentorId/del-review/:reviewId",verifyAdminToken, adminControl.deleteMentorReviews)
+router.get('/courses/search', verifyAdminToken, courseControl.search)
+router.get("/courses/:id/get",verifyAdminToken, courseControl.searchByID),
 router.post("/courses/add",verifyAdminToken, adminControl.addCourse)
 router.delete("/courses/:courseId",verifyAdminToken, adminControl.delCourse)
-router.patch("/courses/:courseId",verifyAdminToken, adminControl.updateCourse)
+router.patch("/courses/:courseId/update",verifyAdminToken, adminControl.updateCourse)
 router.post("/courses/:courseId/add-mentor/:mentorId",verifyAdminToken, adminControl.addMentorToCourse)
 router.post("/courses/:courseId/add-lesson",verifyAdminToken, adminControl.addLessonToCourse)
-router.delete("/mentors/:mentorId/delete",verifyAdminToken, adminControl.deleteMentor)
-router.post("/mentors/:mentorId/add-course/:courseId",verifyAdminToken, adminControl.addMentorCourses)
-router.delete("/mentors/:mentorId/add-course/:courseId",verifyAdminToken, adminControl.deleteMentorCourses)
+router.delete("/courses/:courseId/del-lesson/:lessonId",verifyAdminToken, adminControl.delLessonFromCourse)
+router.post("/videos/add-video", verifyAdminToken, adminControl.addVideo)
+router.delete("/videos/:videoId", verifyAdminToken, adminControl.delVideo)
+router.get("/videos", verifyAdminToken, adminControl.getVideos)
+
+
+
 
 
 module.exports = router;
