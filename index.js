10458a0e881e377d2e768c09379b1078d23ce384
Hamid <hamidjaved615@gmail.com>
2024-08-20 15:25:12 +0500

    Fixing asynchronous operations

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
 const mongoose = require("mongoose");
-require("dotenv").config()
-const userRouter = require("./routes/userRouter")
-const courseRouter = require("./routes/courseRouter")
-const mentorRouter = require("./routes/mentorsRoute")
-const adminRouter = require("./routes/adminRouter")
+require("dotenv").config();
+const userRouter = require("./routes/userRouter");
+const courseRouter = require("./routes/courseRouter");
+const mentorRouter = require("./routes/mentorsRoute");
+const adminRouter = require("./routes/adminRouter");
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
 
@@ -13,22 +13,19 @@ const port = process.env.port || 3000;
 
 app.use(cookieParser());
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 
+async function connectToDatabase() {
+    try {
+        await mongoose.connect(process.env.DB_URL, { useNewUrlParser: true, useUnifiedTopology: true, timeout: 30000 });
+        console.log("Connected To MongoDB");
+    } catch (err) {
+        console.log(`unable to connet with dB : ${err.stack} `);
+    }
+}
 
-app.use("/auth", userRouter)
-app.use("/courses", courseRouter)
-app.use("/mentors", mentorRouter)
-app.use("/admin", adminRouter)
-
-
-mongoose.connect(
-  process.env.DB_URL
-)
-  .then(() => {
-    console.log("Connected To MongoDB"),
-      app.listen(port, () => {
+connectToDatabase().then(() => {
+    app.listen(port, () => {
         console.log("Server running on localhost:" + port);
-      });
-  })
-  .catch((err) => console.log(`unable to connet with dB : ${err.stack} `));
+    });
+});
