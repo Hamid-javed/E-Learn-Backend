a33fd4be4082e4e6d68d915da2df1a8e174a6225
Hamid <hamidjaved615@gmail.com>
2024-08-22 13:15:59 +0500

    Removed

--- index.js
+++ index.js
@@ -13,12 +13,12 @@ const port = process.env.PORT
 
 
 // CORS configuration
-const corsOptions = {
-  origin: 'http://localhost:5173', // Replace with your frontend's origin
-  methods: 'GET,POST,DELETE,PUT',
-  credentials: true, // Allow credentials (cookies) to be sent
-};
-app.use(cors(corsOptions));
+// const corsOptions = {
+//   origin: 'http://localhost:5173', // Replace with your frontend's origin
+//   methods: 'GET,POST,DELETE,PUT',
+//   credentials: true, // Allow credentials (cookies) to be sent
+// };
+// app.use(cors(corsOptions));
 app.use(cors());
 
 app.use(cookieParser());
