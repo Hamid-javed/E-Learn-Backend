08f8b21eb51c2f3f1c73a4157c426ecd215ea6e2
Hamid <hamidjaved615@gmail.com>
2024-08-26 14:10:35 +0500

    FixedReset password API

--- controller/authControl.js
+++ controller/authControl.js
@@ -149,7 +149,7 @@ exports.resetPassword = async (req, res) => {
     const user = await User.findOne({ email: email });
     if (!user) return res.status(404).json({ message: "User not found" });
 
-    if (user.otp !== Number(otp) || user.expireDate < Date.now()) {
+    if (user.otp.otp !== Number(otp) || user.otp.expireDate < Date.now()) {
       return res.status(400).json({ message: "Invalid or expired OTP" });
     }
 
