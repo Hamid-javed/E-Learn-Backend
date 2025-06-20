b65e08050baf0268bbba6d07c0abf82de292d3e5
hamidjaved-dev <hamid@helloworldtech.com>
2025-06-20 10:29:44 +0500

    cors

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ const port = process.env.PORT
 
 const corsOptions = {
   origin: true,
+  origin: ['http://localhost:5173'],
   methods: 'GET,POST,DELETE,PUT,PATCH',
   credentials: true, // Allow credentials (cookies) to be sent
   optionsSuccessStatus: 204
