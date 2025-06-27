d7628f61b723ed37d4ce509a157c532d506eb088
hamidjaved-dev <hamid@helloworldtech.com>
2025-06-27 09:32:15 +0500

    deployment

--- index.js
+++ index.js
@@ -63,7 +63,7 @@ const cors = require("cors");
 const port = process.env.PORT;
 
 const corsOptions = {
-  origin: '*', // or use an array if needed
+  origin: 'https://elearnportal.netlify.app',
   methods: ['GET', 'POST', 'DELETE', 'PUT', 'PATCH'],
   credentials: true,
   optionsSuccessStatus: 204,
